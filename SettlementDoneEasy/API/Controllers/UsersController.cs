dde5dd194725d1d6b86bebb1211f78af785005b0
Sahil Patel <saheeelpatel@gmail.com>
2021-09-28 19:46:48 -0500

    new sln

--- SettlementDoneEasy/API/Controllers/UsersController.cs
+++ SettlementDoneEasy/API/Controllers/UsersController.cs
@@ -2,8 +2,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.EntityFrameworkCore;
-using SDE_Server.Entities;
 using SDE_Server.Hubs;
 using SDE_Server.Infrastructure;
 
