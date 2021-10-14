d6b2199024106e6614655330076f2b75b7f0130b
Blake Hastings <blakehastings@outlook.com>
2021-10-14 12:50:20 -0500

    -Added partial implementation of release statemachine

---
+++ SettlementDoneEasy/Domain/ReleaseStatemachine/ReleaseStateTrigger.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDE_Server.Domain.ReleaseStatemachine
+{
+    public class ReleaseStateTrigger
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
