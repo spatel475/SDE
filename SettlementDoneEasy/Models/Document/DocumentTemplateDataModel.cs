ac40ebaa9d8056d1b2a00048db85c035b758c526
Blake Hastings <blakehastings@outlook.com>
2021-10-16 10:00:27 -0500

    -rebase commit

---
+++ SettlementDoneEasy/Models/Document/DocumentTemplateDataModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDE_Server.Models.Document
+{
+    public class DocumentTemplateDataModel
+    {
+    }
+}
