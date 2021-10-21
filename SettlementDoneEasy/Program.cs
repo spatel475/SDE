42a43933cfc2ce7e4a6bd615a28009b5692e3542
Blake Hastings <blakehastings@outlook.com>
2021-10-21 00:39:47 -0500

    - more stuff

--- SettlementDoneEasy/Program.cs
+++ SettlementDoneEasy/Program.cs
@@ -5,6 +5,7 @@ using System;
 using SDE_Server.Domain.ReleaseStatemachine;
 using System.IO;
 using SDE_Server.Domain.Entities;
+using System.Linq;
 
 namespace SDE_Server
 {
@@ -13,21 +14,23 @@ namespace SDE_Server
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(JsonConvert.SerializeObject(AppSettings.GetSettings()));
-            Console.WriteLine("\nStatemachine Digraph: \n:" + new ReleaseMachine().ToDotMap());
-            Console.WriteLine("\nStatemachine JSON: \n:" + new ReleaseMachine().ToJson());
-            Console.WriteLine(Convert.ToBase64String(File.ReadAllBytes("C:\\Users\\blakehastings\\Downloads\\example-release.pdf")));
+            //Console.WriteLine(JsonConvert.SerializeObject(AppSettings.GetSettings()));
+            //Console.WriteLine("\nStatemachine Digraph: \n:" + new ReleaseMachine().ToDotMap());
+            //Console.WriteLine("\nStatemachine JSON: \n:" + new ReleaseMachine().ToJson());
+            ////Console.WriteLine(Convert.ToBase64String(File.ReadAllBytes("C:\\Users\\blakehastings\\Downloads\\example-release.pdf")));
 
-            using (var db = new SDEDBContext())
-            {
-                db.DocumentTemplateData.Add(new DocumentTemplateData()
-                {
-                    TemplateID=1,
-                    Template = File.ReadAllBytes("C:\\Users\\blakehastings\\Downloads\\example-release.pdf")
-                });
+            //using (var db = new SDEDBContext())
+            //{
+            //    //db.DocumentTemplateData.Add(new DocumentTemplateData()
+            //    //{
+            //    //    TemplateID=1,
+            //    //    Template = File.ReadAllBytes("C:\\Users\\blakehastings\\Downloads\\example-release.pdf")
+            //    //});
 
-                db.SaveChanges();
-            }
+            //    Console.WriteLine(Convert.ToBase64String(db.DocumentTemplateData.Where(x => x.TemplateID == 1).FirstOrDefault().Template));
+
+            //    //db.SaveChanges();
+            //}
 
                 CreateHostBuilder(args).Build().Run();
         }
