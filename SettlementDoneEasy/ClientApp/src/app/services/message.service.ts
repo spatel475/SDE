6f7895d06188c3e1b41340443b05ec0998ba8bed
Z3ypher <29761853+Z3ypher@users.noreply.github.com>
2021-11-09 13:28:45 -0800

    -Added email

--- SettlementDoneEasy/ClientApp/src/app/services/message.service.ts
+++ SettlementDoneEasy/ClientApp/src/app/services/message.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 @Injectable()
 export class MessageService {
 
-  constructor(private_http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   sendMessage(body) {
-    return this._http.post(‘http://localhost:4001/SDE', body);
+    return this.http.post('http://localhost:5001/SDE', body);
   }
 }
